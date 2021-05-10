dce789dd84ec793ee80ad433cc7e8a6be72625be
AJ <amarjits1@hotmail.com>
2021-05-09 22:18:18 -0600

    trying to deal with zeros

--- ProjectEuler/e8_greatest_product_13_digits.py
+++ ProjectEuler/e8_greatest_product_13_digits.py
@@ -24,8 +24,21 @@ staying safe and healthy.
 - AJ
 """
 
-import queue
 from queue import Queue
+
+
+def reset_queue(q: Queue):
+    while not q.empty():
+        q.get()
+
+
+def fill_queue(q: Queue, f_handle):
+    largest = 1
+    for _ in range(13):
+        digit = int(f_handle.read(1))
+        largest *= digit
+        q.put(digit)
+    return largest
 
 
 def largest_product():
@@ -36,32 +49,34 @@ def largest_product():
         largest = 1
 
         # Initialize queue.
-        for _ in range(13):
-            digit = int(file.read(1))
-            largest *= digit
-            q.put(digit)
+        new_largest = fill_queue(q, file)
+        if new_largest > largest:
+            largest = new_largest
 
-        while True:
+        next_digit = file.read(1)
+        while next_digit:
 
-            print(largest)
+            next_digit = int(next_digit)
+            if next_digit == 0:
+                reset_queue(q)
+                new_largest = fill_queue(q, file)
+                if new_largest > largest:
+                    largest = new_largest
+            else:
+                removed = q.get()
+                new_largest = (new_largest / removed) * next_digit
+                if new_largest > largest:
+                    largest = new_largest
 
-            removed_digit = q.get()
-            digit = int(file.read(1))
-            # if not digit:
-            #     break
-            q.put(digit)
-
-            if digit == 0:
-
-            current_product = largest
-            if digit != 0:
-                current_product = (largest / removed_digit) * digit
-
-            if current_product > largest:
-                largest = current_product
+            next_digit = file.read(1)
 
     return largest
 
 
 if __name__ == "__main__":
     print(largest_product())
+    # with open("1000_digit_number.txt", mode="r") as file:
+    #     x = file.read(1)
+    #     while x:
+    #         print(file.read(1))
+    #         x = file.read(1)
